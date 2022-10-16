bc666deaa524d664483113587c08303d54c44b07
Vanessa Andrade <106551477+DevVans@users.noreply.github.com>
2022-10-16 18:48:11 -0300

    atualizações

--- index.js
+++ index.js
@@ -3,11 +3,36 @@ const app = express()
 const queries = require('./src/data/queries.json')
 
 
-//rota para ler o json
-app.get('/', (req, res)=>{
-    return res.json('resposta')
-})  
+//função para encontrar o angulo
+function findAngule(h,m) {
+    let desiredTime = h +":"+ m
+    let querieFound = queries.find((querie) =>{
+        return querie.time === desiredTime;
+})
 
+// verifica a hora que ja foi pesquisada
+if (querieFound){
+    return querieFound['angle']
+} else{
+    //se não foi pesquisada pegue no db e coloque no json
+        //comadno para conectar
+        //comando para achar no DB
+        //select angle from tabledefault t where "hour" = h and "minute" = m;
+            //se não achar na DB
+                //então calcular
+                //add a base de dados
+                //add ao arquivo queri
+            //se achar ele faz o select e pega o angulo 
+                //add ao arquivo querri
+        //associar o valor achar a variavel angulo p/ mostra no navegador
+        return "buscar no DB"
+        //add a nova consulta ao arquivo json
+}
+}
+//rota defaul para ler o json
+app.get('/', (req, res) => {
+    return res.json(queries)
+})
 
 
 //rota caso informe somente a hora
@@ -15,43 +40,20 @@ app.get('/', (req, res)=>{
 app.get('/vin1/rest/clock/:hour' ,(req, res) =>{
     let hour = req.params.hour
     let minutes = "0"
-    let tempo = hour+":"+minutes
-    let querieFound = queries.find ((querie) =>{
-        return querie.time === tempo;
+       
+    return res.json(findAngule(hour,minutes))
     })
-//caso seja verdadeiro
-    if(querieFound){
-        var angulo = querieFound['angle']
-    }else{ 
-        angulo = "buscar no DB"
-        //comando para conectar
-        //comando para achar
-        //associar o valor achar variavel angulo
-    }
-    return res.json(angulo) // add a nova consulta ao arquivo json
-})
+
 
 // rota caso informe hora e minuto
 
 app.get ('/vin1/rest/clock/:hour/minutes' ,(req, res) =>{
     let hour = req.params.hour
     let minutes = req.params.minutes
-    let tempo = hour+":"+minutes
-    let querieFound = queries.find ((querie) =>{
-        return querie.time === tempo;
+
+        return res.json(findAgule(hour,minutes))
     
     })
-//caso seja verdadeiro
-if(querieFound){
-    var angulo = querieFound['angle']
-   }else{ 
-    angulo = "buscar no DB"
-    //comando para conectar
-    //comando para achar
-    //associar o valor achar variavel angulo
-    }
-    return res.json(angulo) // add a nova consulta ao arquivo json
- })
 
  app.listen(8080, () =>{
     console.log('servidor iniciado')
